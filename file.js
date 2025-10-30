a434307ee43d4cfbb3269e2a70fdf4cd72e634b3
sana farooq <sf4563496@gmail.com>
2025-10-30 15:00:15 -0700

    Updating Js file for navbar

---
+++ file.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 60) {
+    nav.classList.add("scrolled");
+    mobileNav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+    mobileNav.classList.remove("scrolled");
+  }
+});
